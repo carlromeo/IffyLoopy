1bd332ea156b26954b8efd3ba2fffe09804d1c65
carlromeo <carlromeo.dogbey@gmail.com>
2019-09-10 10:56:46 -0400

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,43 @@ public class Main {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
 
-        String userInput= "y";
+        String space = "";
+        String response = "y";
+        String userInput = "y";
         boolean userInput2;
 
-
-        while ((!userInput.equalsIgnoreCase("y")) || (userInput.equalsIgnoreCase("Yes)"))) {
-            System.out.println("Are your eyes red?");
-            userInput = keyboard.nextLine();
-            if ((!userInput.equalsIgnoreCase("y")) || (userInput.equalsIgnoreCase("Yes)"))) {
-                userInput2 = false;
-            } else {
-                userInput2 = true;
+        while (!(userInput.equalsIgnoreCase("n") || userInput.equalsIgnoreCase("No"))) {
+            System.out.println("Please type 5 words, hitting enter when you complete each word.");
+            for (int i = 0; i < 5; i++) {
+                space += keyboard.next() + " ";
             }
 
-            if (userInput2 == false) ;
-            System.out.println("Get some sleep!");
+            System.out.println(space);
+            keyboard.nextLine();
 
-        }else{
-            System.out.println("You look great!");
+            System.out.println("Are your eyes red after all that typing?");
+            response = keyboard.next() + " ";
+        if ((response.equalsIgnoreCase("y")) || (response.equalsIgnoreCase("Yes)"))) {
+            userInput2 = true;
+        } else {userInput2 = false;}
+
+        if (userInput2) {
+            System.out.println("Get some sleep!");
+        } else {
+            System.out.println ("You look great!");
         }
         System.out.println("Do you want to play again");
-        userInput= keyboard.nextLine();
+        userInput = keyboard.nextLine();
+        space = "";
+        }
+
+
+    System.out.println("Goodbye then!");
 
     }
 }
+
+
+//while ((userInput.equalsIgnoreCase("y")) || (userInput.equalsIgnoreCase("Yes)"))) {
+//        System.out.println("Are your eyes red?");
+//        userInput = keyboard.nextLine();
